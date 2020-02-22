ca57e853b59cc9a7d30b96a305e52c939973e247
Guillaume Lobet <guillaume.lobet@uclouvain.be>
2020-02-22 07:12:12 +0100

    Initial commit

---
+++ www/ssh_transfers.R
@@ -0,0 +1,15 @@
+
+psw u4m5s!@UA6
+
+ssh globet@130.104.12.110
+
+scp server.R globet@130.104.12.110:/srv/shiny-server/biogo/
+scp ui.R globet@130.104.12.110:/srv/shiny-server/biogo/
+scp global.R globet@130.104.12.110:/srv/shiny-server/biogo/
+
+scp www/database1.sql globet@130.104.12.110:/srv/shiny-server/biogo/www/
+
+scp www/all_quests.pdf globet@130.104.12.110:/srv/shiny-server/biogo/www/
+  
+  
+  
